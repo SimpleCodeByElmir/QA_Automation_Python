9e6b856d1fa730ce64fe3dd9d30660eb1f54c81d
addisona <addisona@student.21-school.ru>
2023-03-30 00:44:08 +0400

    regex

--- src/pytest/strings.py
+++ src/pytest/strings.py
@@ -1,5 +1,20 @@
+import re
+
 def sum_str(a,b):
   return a+b
 
+def regex_str(a):
+  res = re.search(r'abc', a)
+  if res == None:
+    return res
+  else:
+    return "found"
+
 def test_sum_str():
   assert(sum_str("abc","def")) == "abcdef"
+
+def test_regex_str():
+  assert(regex_str("abg")) == None
+
+def test_regex_str_2():
+    assert(regex_str("abg abc")) == "found"
