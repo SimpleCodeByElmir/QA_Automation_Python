fee910404d8b024a75bc27a8b1689eef4a455665
addisona <addisona@student.21-school.ru>
2023-03-29 19:11:33 +0400

    recreation of QA repo

---
+++ src/auto_test-simple.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+
+link = "https://www.google.com/"
+browser = webdriver.Firefox()
+browser.get(link)
+
+search = browser.find_element(By.XPATH, "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input")
+search.send_keys("Globe")
+time.sleep (1)               # I know it's a bad thing to do. But this is just a simple test..
+search.send_keys(Keys.ENTER)
